f80da033b26b1c68497ed4d48ab92873e78070a0
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-10-03 22:34:48 +0300

    Update dependencies

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
 const CreatePage = () => {
-  return <div className="flex justify-center">Create</div>;
+  return (
+    <div className="flex w-full justify-center">
+      <div className="flex flex-col">
+        <div className="h-[55vh] w-[30vw] bg-slate-300">thumbnail</div>
+        <div className="flex h-[25vh] w-[30vw] flex-row bg-red-400">
+          thumbnails
+        </div>
+      </div>
+      <div className="flex h-[80vh] w-[25vw] items-center justify-center bg-green-400">
+        inputs
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
