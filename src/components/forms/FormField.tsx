795fc9a474fd0398bd842bd3fe87f2fa2cc34d2c
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-10-05 19:46:36 +0300

    add textarea compatibility to formfield

--- src/components/forms/FormField.tsx
+++ src/components/forms/FormField.tsx
@@ -1,12 +1,12 @@
 import { IconType } from "react-icons";
 
 type FormFieldProps = {
-  type: "text" | "password" | "email";
+  type: "text" | "password" | "email" | "textarea";
   icon?: IconType;
   useState: [string, React.Dispatch<React.SetStateAction<string>>];
   placeholder?: string;
   onChange?: (
-    e: React.FormEvent<HTMLInputElement>,
+    e: React.FormEvent<HTMLElement>,
     setState: React.Dispatch<React.SetStateAction<string>>,
   ) => void;
   children?: React.ReactNode;
@@ -18,17 +18,29 @@ const FormField = (props: FormFieldProps) => {
   return (
     <div className="textfield  outline-black focus-within:border-black focus-within:outline focus-within:outline-2">
       {Icon && <Icon />}
-      <input
-        className="w-full outline-none"
-        value={state}
-        placeholder={props.placeholder}
-        type={props.type}
-        onChange={(e) =>
-          props.onChange
-            ? props.onChange(e, setState)
-            : setState(e.currentTarget.value)
-        }
-      />
+      {props.type === "textarea" ? (
+        <textarea
+          value={state}
+          placeholder={props.placeholder}
+          onChange={(e) =>
+            props.onChange
+              ? props.onChange(e, setState)
+              : setState(e.currentTarget.value)
+          }
+        />
+      ) : (
+        <input
+          className="w-full outline-none"
+          value={state}
+          placeholder={props.placeholder}
+          type={props.type}
+          onChange={(e) =>
+            props.onChange
+              ? props.onChange(e, setState)
+              : setState(e.currentTarget.value)
+          }
+        />
+      )}
       {props.children}
     </div>
   );
