d6a7fe57fd869bc4038ce34c34a8989851323d7e
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-10-22 21:15:19 +0300

    app name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReduxProvider } from "./redux/provider";
-import Loading from "@/components/Loading";
 import NextTopLoader from "nextjs-toploader";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
     <html lang="ru">
       <body className={`${inter.className}`}>
         <ReduxProvider>
-          <Loading />
           <Navbar />
           <NextTopLoader color="#f72d2d" showSpinner={false} />
           <div className="relative md:px-16 lg:px-32 xl:px-64">{children}</div>
