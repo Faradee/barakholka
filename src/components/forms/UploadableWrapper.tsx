49f0bcf5bb6d054d22095b61195618568fef71d6
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-10-24 15:58:20 +0300

    added reader reinstatiation in uploadablewrapper

--- src/components/forms/UploadableWrapper.tsx
+++ src/components/forms/UploadableWrapper.tsx
@@ -13,9 +13,9 @@ const UploadableWrapper = (props: UploadableWrapperProps) => {
   const [localDim, setLocalDim] = useState<boolean>(false);
   const dragAllow = useAppSelector((state) => state.drag.dragAllow);
   const handleUpload = (e: React.DragEvent) => {
-    const reader = new FileReader();
     if (e.dataTransfer.items) {
       for (let i = 0; i < e.dataTransfer.items.length; i++) {
+        const reader = new FileReader();
         const file = e.dataTransfer.items[i];
         if (file.kind === "file" && file.type.match("^image/")) {
           const image = file.getAsFile() as Blob;
