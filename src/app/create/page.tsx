ffef194fbadcd7b1834ff112a0419fcf61bee524
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-12 23:07:21 +0300

    added meta title to post creator

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,8 +1,11 @@
 import { verifyToken } from "@/actions/userActions";
 import PostEditor from "@/components/postEditor/PostEditor";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
 import React from "react";
-
+export const metadata: Metadata = {
+  title: "Создать объявление",
+};
 const PostCreator = async () => {
   const uuid = await verifyToken();
   if (!uuid) redirect("/");
