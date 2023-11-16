255a02565f26eef62546962d304fd3ef86c7a8ac
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-16 18:56:49 +0300

    added 404 and loading files

---
+++ src/app/404.tsx
@@ -0,0 +1,12 @@
+import LoaderWrapper from "@/components/loader/LoaderWrapper";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <LoaderWrapper>
+      <div className="h-full w-full"></div>
+    </LoaderWrapper>
+  );
+};
+
+export default Loading;
