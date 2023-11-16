ced22ca3426fcf1aae43dcf0690ac34b4fe64768
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-16 19:46:07 +0300

    created custom 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+import LoaderWrapper from "@/components/loader/LoaderWrapper";
+import React from "react";
+
+const Custom404 = () => {
+  return <div>404 does not exist</div>;
+};
+
+export default Custom404;
