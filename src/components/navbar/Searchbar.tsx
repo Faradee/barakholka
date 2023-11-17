19b480074dfd81f231465a289e609a8eb3cc0459
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-17 11:34:53 +0300

    added todo

--- src/components/navbar/Searchbar.tsx
+++ src/components/navbar/Searchbar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import FormField from "../forms/FormField";
+//TODO: LINK SEARCH TO SEARCHPARAMS
 const Searchbar = () => {
   return (
-    <div className="relative mx-5 flex h-full w-full items-center justify-center gap-x-5 p-1">
-      <input className=" h-full w-full border border-black" type="text" />
-      <button>
+    <div className="mx-5 flex h-full w-1/2 items-center justify-center rounded-xl border border-black ">
+      <input
+        className=" h-full w-full flex-grow rounded-xl outline-none"
+        type="text"
+      />
+      <button className="flex h-full w-[50px] flex-grow-0 items-center justify-center border-l border-black">
         <FaSearch size={30} />
       </button>
     </div>
