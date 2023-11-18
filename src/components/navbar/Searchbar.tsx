6935c4cbf5b1c14b73cfe262fd720b23ea349825
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-18 16:48:05 +0300

    linked search to url

--- src/components/navbar/Searchbar.tsx
+++ src/components/navbar/Searchbar.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import { useCallback } from "react";
 import { FaSearch } from "react-icons/fa";
-import FormField from "../forms/FormField";
+import { useRouter, useSearchParams } from "next/navigation";
 //TODO: LINK SEARCH TO SEARCHPARAMS
 const Searchbar = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const query = new FormData(e.currentTarget).get("query")! as string;
+    router.replace("/?" + createQueryString("search", query));
+  };
   return (
-    <div className="mx-5 flex h-full w-1/2 items-center justify-center rounded-xl border border-black ">
+    <form
+      onSubmit={(e) => handleSubmit(e)}
+      className="mx-5 flex h-full w-1/2 items-center justify-center rounded-xl border border-black "
+    >
       <input
-        className=" h-full w-full flex-grow rounded-xl outline-none"
+        className=" mx-2 h-full w-full flex-grow rounded-xl outline-none"
+        name="query"
         type="text"
       />
-      <button className="flex h-full w-[50px] flex-grow-0 items-center justify-center border-l border-black">
+      <button
+        type="submit"
+        className="flex h-full w-[50px] flex-grow-0 items-center justify-center border-l border-black"
+      >
         <FaSearch size={30} />
       </button>
-    </div>
+    </form>
   );
 };
 
