7add687a962067bbd41150baf353141ccb4abb56
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-21 10:10:45 +0300

    added responsiveness to searchbar

--- src/components/navbar/Searchbar.tsx
+++ src/components/navbar/Searchbar.tsx
@@ -1,13 +1,16 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef } from "react";
 import { CiSearch } from "react-icons/ci";
 import { useSearchParams, useRouter } from "next/navigation";
 import { AiOutlineClose } from "react-icons/ai";
-//TODO: ADD RESPONSIVENESS
+import { useClickOutside } from "../containers/DropDownContainer";
+import { FaArrowLeft } from "react-icons/fa6";
 const Searchbar = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const [query, setQuery] = useState<string>("");
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const formRef = useRef(null);
   const handleSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
@@ -20,37 +23,55 @@ const Searchbar = () => {
     },
     [searchParams, query, router],
   );
+  useClickOutside(formRef, () => setIsActive(false));
   return (
-    <form
-      onSubmit={(e) => handleSubmit(e)}
-      className="mx-5 flex h-full w-1/2 items-center justify-center rounded-xl"
-    >
-      <label className="relative flex h-full w-full flex-grow rounded-s-xl border border-black font-normal  outline-none focus-within:border-blue-500 ">
-        <input
-          className="h-full w-full rounded-s-xl px-2 outline-none"
-          value={query}
-          name="query"
-          type="text"
-          onChange={(e) => setQuery(e.currentTarget.value)}
-        />
-        {query && (
-          <div
-            onClick={() => setQuery("")}
-            className="relative flex cursor-pointer items-center justify-center rounded-full p-1 before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200"
-          >
-            <AiOutlineClose size={30} />
-          </div>
-        )}
-      </label>
-
+    <>
       <button
-        type="submit"
-        title="Поиск"
-        className="flex h-full w-[50px] flex-grow-0 items-center justify-center rounded-e-xl border border-l-0 border-black active:bg-slate-50"
+        className="relative flex items-center justify-center rounded-full p-1 before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200"
+        onClick={() => setIsActive(true)}
       >
         <CiSearch size={30} />
       </button>
-    </form>
+      {isActive && (
+        <form
+          ref={formRef}
+          onSubmit={(e) => handleSubmit(e)}
+          className="absolute z-50 -mx-5 flex  h-4/5 w-full min-w-[30px] items-center justify-center rounded-xl bg-white p-1 md:static md:mx-5 md:h-full md:w-1/2 md:px-0"
+        >
+          <button
+            onClick={() => setIsActive(false)}
+            className="relative mr-2 flex items-center justify-center rounded-full bg-white before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200"
+          >
+            <FaArrowLeft size={30} />
+          </button>
+          <label className="relative flex h-full w-full flex-grow rounded-s-xl border border-black font-normal  outline-none focus-within:border-blue-500 ">
+            <input
+              className="h-full w-full rounded-s-xl px-2 outline-none"
+              value={query}
+              name="query"
+              type="text"
+              onChange={(e) => setQuery(e.currentTarget.value)}
+            />
+            {query && (
+              <div
+                onClick={() => setQuery("")}
+                className="relative flex cursor-pointer items-center justify-center rounded-full p-1 before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200"
+              >
+                <AiOutlineClose size={30} />
+              </div>
+            )}
+          </label>
+
+          <button
+            type="submit"
+            title="Поиск"
+            className="flex h-full w-[50px] flex-grow-0 items-center justify-center rounded-e-xl border border-l-0 border-black bg-white active:bg-slate-50"
+          >
+            <CiSearch size={30} />
+          </button>
+        </form>
+      )}
+    </>
   );
 };
 
