aaf75685aa6631e9473e709f877445b6a3309f8a
Vladislav Nuyanzin <42471298+Faradee@users.noreply.github.com>
2023-11-22 19:59:31 +0300

    added correct hiding behavior on mobile

--- src/components/navbar/Searchbar.tsx
+++ src/components/navbar/Searchbar.tsx
@@ -5,6 +5,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { AiOutlineClose } from "react-icons/ai";
 import { useClickOutside } from "../containers/DropDownContainer";
 import { FaArrowLeft } from "react-icons/fa6";
+//TODO: FIX HIDING
 const Searchbar = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -27,23 +28,28 @@ const Searchbar = () => {
   return (
     <>
       <button
-        className="relative flex items-center justify-center rounded-full p-1 before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200"
+        className={`relative flex items-center justify-center rounded-full p-1 before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200 md:hidden`}
         onClick={() => setIsActive(true)}
       >
         <CiSearch size={30} />
       </button>
-      {isActive && (
-        <form
-          ref={formRef}
-          onSubmit={(e) => handleSubmit(e)}
-          className="absolute z-50 -mx-5 flex  h-4/5 w-full min-w-[30px] items-center justify-center rounded-xl bg-white p-1 md:static md:mx-5 md:h-full md:w-1/2 md:px-0"
+
+      <form
+        ref={formRef}
+        onSubmit={(e) => handleSubmit(e)}
+        className={`absolute z-50 -mx-5   h-full w-full min-w-[30px] items-center justify-center rounded-xl bg-white p-1 md:static md:mx-5 md:flex md:w-1/2 md:px-0 ${
+          isActive ? "flex" : "hidden"
+        }`}
+      >
+        <button
+          onClick={() => setIsActive(false)}
+          className={`relative mr-2 flex items-center justify-center rounded-full bg-white before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200 md:hidden ${
+            isActive ? "visible" : "hidden"
+          }`}
         >
-          <button
-            onClick={() => setIsActive(false)}
-            className="relative mr-2 flex items-center justify-center rounded-full bg-white before:absolute before:h-[50px] before:w-[50px] before:rounded-full before:opacity-70 hover:before:bg-slate-100 active:before:bg-slate-200"
-          >
-            <FaArrowLeft size={30} />
-          </button>
+          <FaArrowLeft size={30} />
+        </button>
+        <div className="flex h-3/5 w-full md:h-full">
           <label className="relative flex h-full w-full flex-grow rounded-s-xl border border-black font-normal  outline-none focus-within:border-blue-500 ">
             <input
               className="h-full w-full rounded-s-xl px-2 outline-none"
@@ -61,7 +67,6 @@ const Searchbar = () => {
               </div>
             )}
           </label>
-
           <button
             type="submit"
             title="Поиск"
@@ -69,8 +74,8 @@ const Searchbar = () => {
           >
             <CiSearch size={30} />
           </button>
-        </form>
-      )}
+        </div>
+      </form>
     </>
   );
 };
